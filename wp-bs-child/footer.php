0c2916a33ad0aabc688660d12990bd0193c73489
minimallinux <anthonymcgowan1@gmail.com>
2018-01-17 12:29:59 +0000

    Add Footer

--- wp-bs-child/footer.php
+++ wp-bs-child/footer.php
@@ -1,16 +1,114 @@
-<footer class="container">
+<div class="container">
   <hr/>
-
-	<div class="row">
+  <div class="row">
     <?php dynamic_sidebar('footer-widget-area'); ?>
   </div>
+</div>
+<!--Footer-->
+<footer class="footer-4">
+			<div class="container">
+				<div class="row mb-5 text-center text-md-left">
+					<div class="col-md-3 col-lg-4 mb-3">
+						<a class="logo" href="index.php">Webmobapps</a>
+						<div>&nbsp;</div>
+						<a target="_blank" href="http://shareasale.com/r.cfm?b=1041259&amp;u=1549027&amp;m=41388&amp;urllink=&amp;afftrack="><img class="img-fluid" src="images/wplift.jpg" width="220px" height="220px" border="0" /></a>
+					</div>
+					<div class="col-md-3 offset-lg-2 col-lg-2 pt-2">
+						<h5>Site</h5>
+						<ul class="nav-footer">
+							<li class="nav-item">
+								<a class="nav-link" href="index.php">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="contact.php">Contact</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="privacy.php">Privacy</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="terms.php">Terms</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="disclaimer.php">Disclaimer</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="copyright.php">Copyright</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-lg-2 pt-2">
+						<h5>Tools 1</h5>
+						<ul class="nav-footer">
+							<li class="nav-item">
+								<a class="nav-link" href="https://nodejs.org/">NodeJS</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://expressjs.com/">ExpressJS</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="resources/10Frameworks.pdf">Frameworks</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://cordova.apache.org/">Cordova</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://www.javascripting.com/">Javascript</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="http://getbootstrap.com/">Bootstrap</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-lg-2 pt-2">
+						<h5>Tools 2</h5>
+						<ul class="nav-footer">
+							<li class="nav-item">
+								<a class="nav-link" href="https://wordpress.org/">Wordpress</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://woocommerce.com">Woocommerce</a>
+							</li>
+					        <li class="nav-item">
+								<a class="nav-link" href="https://laravel.com/docs/5.5/blade">Blade</a>
+							</li>
 
-  <div class="row">
-    <div class="col">
-      <p class="text-center">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
-    </div>
-  </div>
-</footer>
+
+							<li class="nav-item">
+								<a class="nav-link" href="https://www.mysql.com/">MySQL</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://www.mongodb.com/">MongoDB</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="https://www.postgresql.org/">PostgreSQL</a>
+							</li>
+						</ul>
+					</div>
+					
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="divider"></div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 text-center text-md-left mt-2 mb-3 pt-1">
+						<p class="copyright">Copyright &copy; 2018 Webmobapps All rights reserved.</p>
+					</div>
+					<div class="col-md-6 text-center text-md-right mb-4">
+						<ul class="social">
+							<li><a href="https://www.facebook.com/Webmobapps-294063081057563/" title="Facebook" class="fa icon-facebook"></a></li>
+							<li><a href="https://twitter.com/WebMobApps1" title="Twitter" class="fa icon-twitter"></a></li>
+							<li><a href="https://www.linkedin.com/in/paul-anthony-mcgowan-66a71990/" title="Linkedin" class="fa icon-linkedin"></a></li>
+							<li><a href="https://plus.google.com/b/115912604147779772368/?pageId=115912604147779772368" title="Google+" class="fa icon-google"></a></li>
+							<li><a href="https://www.pinterest.co.uk/paulanthonymcgo/" title="Pinterest" class="fa icon-pinterest"></a></li>
+							<li><a href="https://www.youtube.com/channel/UCEsYvFMWMPE-Qlyjdt0nDUg/videos" title="YouTube" class="fa icon-youtube"></a></li>
+              <div class="clear"></div>
+						</ul>
+			    </div>
+        </div>
+			</div>
+		</footer>
 
 
 <?php wp_footer(); ?>
